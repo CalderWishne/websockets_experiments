5ed7caa6c2b21d505a8e972cbd4d1800afddade1
Calder Wishne <apprentice@dbc-chicago-002.local>
2014-04-12 11:48:30 -0500

    added rails skeleton

---
+++ websockets_experiment/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WebsocketsExperiment::Application.config.secret_key_base = '3774e2cb1db1f7446901a1daa97aa0472cf5ce1271215a4b8767b9453fb71f83a63b9d82fc09c7e10074409ae6ac157341135f3a3722b271affa896c5eac6bcb'
---
+++ websockets_experiment/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+WebsocketsExperiment::Application.config.session_store :cookie_store, key: '_websockets_experiment_session'
